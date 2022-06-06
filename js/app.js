913b1ea753bc886ffbd249f6c55f780e3d7ca8d2
Harshal141 <harshalmukundapatil@gmail.com>
2022-06-06 16:35:32 +0530

    Escape

--- js/app.js
+++ js/app.js
@@ -1,9 +1,12 @@
+//--------------------------------------
+// Search Page js
+//--------------------------------------
+
 console.log("yo")
 let count1 = document.querySelector('.count1')
 let count2 = document.querySelector('.count2')
 let count3 = document.querySelector('.count3')
 let count4 = document.querySelector('.count4')
-
 
 // geting counters
 for(let i=0;i<86;++i){
@@ -19,6 +22,18 @@ for(let i=0;i<18;++i){
     setTimeout(function(){count4.innerText=i},20+10*i)
 }
 
-// calling bootstrap
+// calling search
 let spec = document.querySelector('.spec').value
 let equip = document.querySelector('.equip').value
+
+
+
+
+
+
+
+
+
+//--------------------------------------
+// Comp Page js
+//--------------------------------------
